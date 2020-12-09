743c9ac5061677b0b1133a8fa0e5dc42c6b915c3
cherima <cherimagabilmomin@gmail.com>
2020-12-09 16:02:42 +0530

    inserted

--- xyz.py
+++ xyz.py
@@ -59,3 +59,67 @@ else:
 print("The maximum of 3 numbers are:",largest)
 
 
+#que7.You are given a cubic dice with 6 faces. All the individual faces have a number printed on them. The numbers are in the range of 1 to 6,
+ like any ordinary dice. Input a dice face number, and output the opposite face number.
+ i=0
+while (i>1) or (i<6):
+  i=int(input("Enter any number="))
+  num=7-i
+print(num)
+
+
+#Find the average of numbers in a list
+def average(num):
+    sum= 0
+    for t in num:
+        sum= sum + t           
+
+    avg = sum/ len(num)
+    return avg
+
+print("The average is", average([18,25,3,41,5]))
+
+
+#Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, 
+between 2000 and 3200 (both included)
+for i in range(1500, 2701):
+    if (i%7==0) and (i%5==0):
+        nl.append(str(x))
+print (','.join(nl))
+
+
+#Perform operations on complex type values.(ie … 10+1.5j)
+import cmath
+a=10+1.5j
+cmath.polar(a)
+
+#Program for minimum of 3 numbers.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+ 
+if (num1 < num2) and (num1 < num3):
+   smallest = num1
+elif (num2 < num1) and (num2 < num3):
+   smallest = num2
+else:
+   smallest= num3
+ 
+print("minimum of 3 numbers are:",smallest)
+
+#Find sum of digits in a number
+a=int(input("Enter any number:"))
+b=int(input("Enter any number:"))
+sum=a+b
+print("sum of digits:",sum)
+
+#Find if a list is sorted in increasing order
+numlist=[]
+num=int(input("Enter the total number of list:"))
+for i in range(1,num+1):
+  value=int(input("Enter the value of %d element:" %i))
+  numlist.append(value)
+   
+
+
+
